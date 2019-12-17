d39e3c3f4a981dde13a686b011825ce07f253257
Maria Giuseppina Nestola <nestom@usi.ch>
2019-12-17 12:50:56 +0100

    added postprocVolume and daint run

---
+++ src/postprocessors/ElementIntegralVolumePostprocessor.C
@@ -0,0 +1,62 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "ElementIntegralVolumePostprocessor.h"
+#include "FractureUserObject.h"
+
+registerMooseObject("parrotApp", ElementIntegralVolumePostprocessor);
+
+template <>
+InputParameters
+validParams<ElementIntegralVolumePostprocessor>()
+{
+  InputParameters params = validParams<ElementIntegralPostprocessor>();
+  params.addRequiredCoupledVar("variable", "The name of the variable that this object operates on");
+  params.addRequiredParam<std::string>("fractureMeshModifier","fractureMeshModifier");
+  params.addRequiredParam<int>("fractureRegionId","fractureRegionId");
+  return params;
+}
+
+ElementIntegralVolumePostprocessor::ElementIntegralVolumePostprocessor(
+    const InputParameters & parameters)
+  : ElementIntegralPostprocessor(parameters),
+    MooseVariableInterface<Real>(this,
+                                 false,
+                                 "variable",
+                                 Moose::VarKindType::VAR_ANY,
+                                 Moose::VarFieldType::VAR_FIELD_STANDARD),
+   _meshModifierName(getParam<std::string>("fractureMeshModifier")), 
+   _regionId(getParam<int>("fractureRegionId")),
+   _u(coupledValue("variable"))
+{
+  addMooseVariableDependency(mooseVariable());
+}
+
+Real
+ElementIntegralVolumePostprocessor::computeQpIntegral()
+{
+  // std::string _meshModifierName;
+
+  // params.addRequiredParam<std::string>("fractureMeshModifier","fractureMeshModifier");
+
+  // _meshModifierName(getParam<std::string>("fractureMeshModifier")),
+
+   MeshModifier const & _myMeshModifier( _app.getMeshModifier( _meshModifierName.c_str()) );
+
+   FractureUserObject const & _fractureUserObject( dynamic_cast<FractureUserObject const &>(_myMeshModifier) );
+  
+   Real bound = 0.0;
+   bool check = _fractureUserObject.isInsideRegion(_q_point[_qp], _regionId, bound);
+
+   if(check==true)
+         1.0;
+   else
+       return 0.0;
+}
+

901ee1edc97c403f768b03ffdf985553b1952451
Marco Favino <favinom@usi.ch>
2019-12-23 17:01:12 +0100

    ciao Maria

--- examples/paperAMR/regular_2D/myclean.sh
+++
@@ -1,8 +0,0 @@
-rm -rf *xda
-rm -rf refined*
-rm -rf verifiedMesh.e
-rm -rf DiffusionOut*
-rm -rf AdvectionOut*
-rm -rf first.txt
-rm -rf second.txt
-rm -rf third.txt
--- examples/paperAMR/regular_2D/uniteFile.sh
+++
@@ -1,9 +0,0 @@
-filename=$1;
-
-cat first.txt > ${filename}
-cat second.txt >> ${filename}
-cat third.txt >> ${filename}
-
-rm -rf first.txt
-rm -rf second.txt
-rm -rf third.txt

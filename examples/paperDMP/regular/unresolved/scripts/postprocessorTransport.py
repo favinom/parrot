b120b50b828be7d9d7bd0494d797ac82a93e4405
Marco Favino <favinom@usi.ch>
2020-06-17 00:52:10 +0200

    ciao

---
+++ examples/paperDMP/regular/unresolved/scripts/postprocessorTransport.py
@@ -0,0 +1,56 @@
+from paraview.simple import *
+import os, sys
+
+inputName=sys.argv[1]
+case=inputName[12:-2]
+print(case)
+
+pressureLineAName='./pres_line_A'+case+'.csv'
+pressureLineBName='./pres_line_B'+case+'.csv'
+pressureLineCName='./pres_line_C'+case+'.csv'
+
+CMLineAName='./CM_line_A'+case+'.csv'
+CMLineBName='./CM_line_B'+case+'.csv'
+CMLineCName='./CM_line_C'+case+'.csv'
+
+reader = ExodusIIReader(FileName=inputName)
+
+reader.GenerateObjectIdCellArray = 0
+reader.GenerateGlobalElementIdArray = 0
+reader.GenerateGlobalNodeIdArray = 0
+reader.GlobalVariables = []
+
+plotOverLine1 = PlotOverLine(Input=reader, Source='High Resolution Line Source')
+
+# A line
+plotOverLine1.Source.Point1 = [0.0, 0.7, 0.0]
+plotOverLine1.Source.Point2 = [1.0, 0.7, 0.0]
+
+reader.PointVariables = ['CM']
+
+SaveData(CMLineAName, proxy=plotOverLine1, Precision=12, WriteTimeSteps=1)
+
+reader.PointVariables = ['pressure']
+SaveData(pressureLineAName, proxy=plotOverLine1, Precision=12, WriteTimeSteps=0)
+
+# B line
+plotOverLine1.Source.Point1 = [0.5, 0.0, 0.0]
+plotOverLine1.Source.Point2 = [0.5, 1.0, 0.0]
+
+reader.PointVariables = ['CM']
+SaveData(CMLineBName, proxy=plotOverLine1, Precision=12, WriteTimeSteps=1)
+
+
+reader.PointVariables = ['pressure']
+SaveData(pressureLineBName, proxy=plotOverLine1, Precision=12, WriteTimeSteps=0)
+
+# C line
+plotOverLine1.Source.Point1 = [0.0, 0.1, 0.0]
+plotOverLine1.Source.Point2 = [0.9, 1.0, 0.0]
+
+reader.PointVariables = ['CM']
+SaveData(CMLineCName, proxy=plotOverLine1, Precision=12, WriteTimeSteps=1)
+
+
+reader.PointVariables = ['pressure']
+SaveData(pressureLineCName, proxy=plotOverLine1, Precision=12, WriteTimeSteps=0)

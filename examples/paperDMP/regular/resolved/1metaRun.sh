8c15af62c16ae13d552e8dc39be82c05aa9c5d7b
Marco Favino <favinom@usi.ch>
2020-06-19 09:09:19 +0200

    changed

--- examples/paperDMP/regular/resolved/1metaRun.sh
+++ examples/paperDMP/regular/resolved/1metaRun.sh
@@ -1,48 +1,51 @@
 #!/bin/bash
 
-action="./2createMesh.sh"
+# action="./2createMesh.sh"
+action="./3runScript.sh"
 
-$action   80  1
+correction=2
+
+$action   80  1 $correction
 exit
-$action   80  2
-$action   80  4
-$action   80  8
-$action   80 16
+$action   80  2 $correction
+$action   80  4 $correction
+$action   80  8 $correction
+$action   80 16 $correction
 
-$action  160  1
-$action  160  2
-$action  160  4
-$action  160  8
-$action  160 16
+$action  160  1 $correction
+$action  160  2 $correction
+$action  160  4 $correction
+$action  160  8 $correction
+$action  160 16 $correction
 
-$action  320  1
-$action  320  2
-$action  320  4
-$action  320  8
-$action  320 16
+$action  320  1 $correction
+$action  320  2 $correction
+$action  320  4 $correction
+$action  320  8 $correction
+$action  320 16 $correction
 
-$action  640  1
-$action  640  2
-$action  640  4
-$action  640  8
-$action  640 16
+$action  640  1 $correction
+$action  640  2 $correction
+$action  640  4 $correction
+$action  640  8 $correction
+$action  640 16 $correction
 
-$action 1280  1
-$action 1280  2
-$action 1280  4
-$action 1280  8
-$action 1280 16
+$action 1280  1 $correction
+$action 1280  2 $correction
+$action 1280  4 $correction
+$action 1280  8 $correction
+$action 1280 16 $correction
 
-$action 4000  1
-$action 4000  2
-$action 4000  4
-$action 4000  8
-$action 4000 16
+$action 4000  1 $correction
+$action 4000  2 $correction
+$action 4000  4 $correction
+$action 4000  8 $correction
+$action 4000 16 $correction
 
 exit
 
-$action 8000  1
-$action 8000  2
-$action 8000  4
-$action 8000  8
-$action 8000 16
+$action 8000  1 $correction
+$action 8000  2 $correction
+$action 8000  4 $correction
+$action 8000  8 $correction
+$action 8000 16 $correction
